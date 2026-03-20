761a235136b61ee2633f429ff31b41d2051eca99
Jason Lewis <lewisjc@my.lanecc.edu>
2026-03-19 23:47:24 -0700

    lab 02 prod commit

---
+++ GC_Board_Game_Cats/GC_Board_Game_Cats/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GC_Board_Game_Cats.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Placeholder page for links to top posts
+            return View();
+        }
+
+        public IActionResult AboutAuthor()
+        {
+            // About the author page
+            return View();
+        }
+
+    }
+}
---
+++ GC_Board_Game_Cats/GC_Board_Game_Cats/Controllers/GalleryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GC_Board_Game_Cats.Controllers
+{
+    public class GalleryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
